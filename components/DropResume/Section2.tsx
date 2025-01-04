15d2743e14e8a629eda26db32b976b429b808149
SahilSharma2911 <sahilsharmarksh@gmail.com>
2025-01-04 14:30:57 +0530

    drop resume 2nd page done complete

--- components/DropResume/Section2.tsx
+++ components/DropResume/Section2.tsx
@@ -1,19 +1,20 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Section2 = () => {
     return (
-        <div className='pt-10 rounded-lg mt-10 px-4 lg:px-14 bg-[#FFFBFB] space-y-6'>
+        <div className='pt-6 md:pt-10 pb-10 rounded-lg mt-8 md:mt-10 px-1.5 md:px-4 lg:px-14 bg-[#FFFBFB]'>
 
             {/* Bird image  */}
             <div className=' flex justify-center'>
-                <Image src={"/Images/bird.png"} width={150} height={150} alt="icon" className='mb-7 -mr-1' />
+                <Image src={"/Images/bird.png"} width={150} height={150} alt="icon" className='' />
             </div>
 
             {/* info  */}
-            <div className=' w-full md:w-7/12 mx-auto'>
-                <h3 className=' font-caveat text-center text-[2rem] text-red font-bold'>Yay,[First Name],</h3>
-                <p className=' text-center font-sans text-[1.5rem]'>Thank you for dropping your resume! Your profile has been successfully created and updated. You have also subscribed to receive job alerts from [business_name].</p>
+            <div className=' w-full md:w-8/12 mx-auto mt-2'>
+                <h3 className='font-caveat text-center text-[1.6rem] md:text-[2rem] text-red font-bold'>Yay,[First Name],</h3>
+                <p className='mt-3 text-center font-sans text-xl md:text-2xl'>Thank you for dropping your resume! Your profile has been successfully created and updated. You have also subscribed to receive job alerts from [business_name].</p>
                 <p className=' text-[#585E68] font-sans text-center mt-3'>
                     <span className=' font-bold'> What ‘s next?</span>
                     <br />
@@ -22,30 +23,29 @@ const Section2 = () => {
             </div>
 
             {/* other links  */}
-            <div className=' flex gap-4 justify-center'>
-
+            <div className='mt-5 flex gap-5 justify-center'>
                 <div>
-                    <Image src={"/Images/googlePlay.png"} alt='image' width={90} height={40} />
+                    <Image src={"/Images/googlePlay.png"} alt='image' width={120} height={120} />
                 </div>
                 <div>
-                    <Image src={"/Images/appStore.png"} alt='image' width={90} height={40} />
+                    <Image src={"/Images/appStore.png"} alt='image' width={120} height={120} />
                 </div>
-
             </div>
 
 
             {/* submit buttons  */}
 
-            <div className=' flex md:flex-row flex-col gap-4 justify-center'>
-
-                <button className="text-sm bg-[#979797] hover:bg-[#868686] transition duration-300 rounded-lg py-2.5 px-8 text-white font-sans text-[0.8rem]">
-                    Skip for now
-                </button>
-
-                <button className="text-sm bg-[#D9292F] hover:bg-[#b22225] transition duration-300 rounded-lg py-2.5 px-8 text-white text-[0.8rem] font-sans">
-                    Go to step 4
-                </button>
-
+            <div className='mt-7 flex flex-row gap-4 justify-center'>
+                <Link href={"/resume/your-preferences"}>
+                    <button className="text-sm bg-[#979797] hover:bg-[#868686] transition duration-300 rounded-lg py-2.5 px-5 md:px-12 text-white font-sans text-[0.8rem]">
+                        Skip for now
+                    </button>
+                </Link>
+                <Link href={"/resume/your-preferences"}>
+                    <button className="text-sm bg-[#D9292F] hover:bg-[#b22225] transition duration-300 rounded-lg py-2.5 px-5 md:px-12 text-white text-[0.8rem] font-sans">
+                        Go to step 4
+                    </button>
+                </Link>
             </div>
         </div>
     )
